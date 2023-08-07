944b0e283b29cbddd2b554223fdebb5f75586db5
Sita Ram Thing <sitaramthing667@gmail.com>
2023-08-07 21:31:37 +0545

    create the new database

--- app/src/main/java/com/record/record/feature/database/AppDatabase.kt
+++ app/src/main/java/com/record/record/feature/database/AppDatabase.kt
@@ -8,9 +8,11 @@ import androidx.room.RoomDatabase
 @Database(entities = [UserEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
+
     companion object {
         private const val DB_NAME: String = "userDB"
         private var INSTANCE: AppDatabase? = null
+
         fun getInstance(context: Context): AppDatabase{
             if (INSTANCE == null) {
                 INSTANCE = Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME)
@@ -19,6 +21,5 @@ abstract class AppDatabase : RoomDatabase() {
             }
             return INSTANCE as AppDatabase
         }
-
     }
 }
